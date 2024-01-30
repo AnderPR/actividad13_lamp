ae2b409d3a384137ce3e0da12d49e64ce986c9a9
AnderPR <92815931+AnderPR@users.noreply.github.com>
2024-01-30 10:37:53 +0100

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Actividad 1.3.1</title>
+    </head>
+    <body>
+        <?php echo "<h1>Actividad 1.3.1: LAMP Sever. Autor: Ander Pelayo Remesal</h1>"; ?>   
+    </body>
+</html>
